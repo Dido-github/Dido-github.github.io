f3213efa2b9f407136d748ba5eae3c922089a78a
Dido-github <51771425+Dido-github@users.noreply.github.com>
2019-06-14 15:53:55 +0800

    Add files via upload

---
+++ myworks/data.js
@@ -0,0 +1,1 @@
+var myData={designer:{name:'刘纯',phone:'18273172707',tag:["#爱设计 #爱生活 #就是美 "],bg:'#26241e'},gallery:[{src:"myworks/1 (3).jpg",w:2500,h:1618},{src:"myworks/1 (1).jpg",w:3264,h:2448},{src:"myworks/1 (4).jpg",w:2854,h:3539},{src:"myworks/1 (2).jpg",w:6000,h:4000},]}
