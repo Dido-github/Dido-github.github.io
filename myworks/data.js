d8d5ccde891eaa80110252a955bcee06908f28ab
Dido-github <51771425+Dido-github@users.noreply.github.com>
2019-08-25 22:37:36 +0800

    Update data.js

--- myworks/data.js
+++ myworks/data.js
@@ -1,1 +1,37 @@
-var myData={designer:{name:'刘纯',phone:'18273172707',tag:["#爱设计 #爱生活 #就是美 "],bg:'#26241e'},gallery:[{src:"myworks/1 (3).jpg",w:2500,h:1618},{src:"myworks/1 (1).jpg",w:3264,h:2448},{src:"myworks/1 (4).jpg",w:2854,h:3539},{src:"myworks/1 (2).jpg",w:6000,h:4000},]}
+var myData={designer:{name:'刘纯',phone:'18273172707',tag:["#爱设计 #爱生活 #就是美 "],bg:'#26241e'},gallery:[{
+	src: "myworks/3.jpg",
+	w: 2500,
+	h: 1618
+}, {
+	src: "myworks/4.jpg",
+	w: 3264,
+	h: 2448
+}, {
+	src: "myworks/5.jpg",
+	w: 2854,
+	h: 3539
+}, {
+	src: "myworks/6.jpg",
+	w: 6000,
+	h: 4000
+}, {
+	src: "myworks/7.jpg",
+	w: 6000,
+	h: 4000
+}, {
+	src: "myworks/8.jpg",
+	w: 6000,
+	h: 4000
+}, {
+	src: "myworks/9.jpg",
+	w: 6000,
+	h: 4000
+}, {
+	src: "myworks/10.jpg",
+	w: 6000,
+	h: 4000
+}, {
+	src: "myworks/11.jpg",
+	w: 6000,
+	h: 4000
+}]}
